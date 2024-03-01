dc4b4f63e22f85f4f30e2d23e1c89ebcc36d1590
Raj Aryan <rajxryn@gmail.com>
2024-03-02 03:02:11 +0530

    change build
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- postbuild.sh
+++ postbuild.sh
@@ -4,13 +4,13 @@
 curl -L -O https://github.com/vercel/geist-font/releases/download/1.1.0/Geist.Mono.zip
 
 # Step 2: Unzip the font archive
-unzip Geist.Mono.zip -d Geist.Mono_temp
+unzip Geist.Mono.zip -d Geist.Mono_temp_d
 
 # Step 3: Create directory if not exists
 mkdir -p ./vercel/output/functions/api/card.func/.next/server/app/api/card/fonts
 
 # Step 4: Move the font file to the desired directory
-mv Geist.Mono_temp/*.ttf ./vercel/output/functions/api/card.func/.next/server/app/api/card/fonts/
+mv Geist.Mono_temp_d/Geist.Mono/GeistMonoVariableVF.ttf ./vercel/output/functions/api/card.func/.next/server/app/api/card/fonts/
 
 # Step 5: Cleanup
-rm -rf Geist.Mono_temp && rm Geist.Mono.zip
+rm -rf Geist.Mono_temp_d && rm Geist.Mono.zip
