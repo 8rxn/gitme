db6b39a2fb14ff3a0dce8b998e7377abced8823e
Raj Aryan <rajxryn@gmail.com>
2024-03-03 23:01:04 +0530

    tweak next config
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (cfg, options = {}) => {
-        cfg.externals.push('sharp')
-        const { webpack } = options
-        const regex = /^sharp$/
-        cfg.plugins.push(new webpack.IgnorePlugin({
-          resourceRegExp: regex,
-        }))
-      }
-};
+      cfg.externals.push('sharp')
+      const { webpack } = options
+      const regex = /^sharp$/
+      cfg.plugins.push(new webpack.IgnorePlugin({
+        resourceRegExp: regex,
+      }))
+      return cfg
+    }
+
+    
+  }
 
 export default nextConfig;
