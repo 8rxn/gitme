228217f18e4353d6aacceef798a6c008e814a6e9
Raj Aryan <rajxryn@gmail.com>
2024-03-04 19:00:08 +0530

    sitemap, robots
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
+    sitemap: 'https://gitme.rajaryan.com/sitemap.xml',
+  }
+}
